61906e3e87ad8fbeb4557e8cb60081e7a4962bb7
Julio Fernandes <jfjunior@zapcorp.com.br>
2018-03-29 16:56:48 -0300

    removendo pods para entrar no gitignore

--- Pods/Target Support Files/Pods-Carona/Pods-Carona-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Carona : NSObject
-@end
-@implementation PodsDummy_Pods_Carona
-@end
--- Pods/Target Support Files/Pods-CaronaTests/Pods-CaronaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CaronaTests : NSObject
-@end
-@implementation PodsDummy_Pods_CaronaTests
-@end
